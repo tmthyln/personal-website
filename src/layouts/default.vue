93dd9a33990b88eb8eeeda652f938f03a12f9dd0
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-01-30 23:28:25 -0500

    keep footer at bottom of page

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,7 +3,7 @@ import NavigationBar from "../components/NavigationBar.vue";
 </script>
 
 <template>
-  <div class="is-flex is-flex-direction-column">
+  <div class="is-flex is-flex-direction-column" style="min-height: 100vh;">
 
     <NavigationBar/>
 
@@ -11,7 +11,7 @@ import NavigationBar from "../components/NavigationBar.vue";
       <RouterView/>
     </main>
 
-    <footer class="footer">
+    <footer class="footer mt-auto">
       <div class="content has-text-centered">
         <p>
           © 2019-{{ new Date().getFullYear() }} Timothy Lin.
