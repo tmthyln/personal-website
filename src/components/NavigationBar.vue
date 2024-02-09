9cb2bb2eefc57b1a7434c5cfcea10e66e46ea335
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-02-09 13:04:42 -0500

    clean up links dropdown

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,6 +2,22 @@
 import {ref} from "vue";
 
 const expandMenu = ref(false);
+
+const placeLinks = [
+    {
+        text: 'GitHub',
+        link: 'https://github.com/tmthyln/',
+        icon: '<i class="fab fa-fw fa-github-square" aria-hidden="true">',
+    }, {
+        text: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/tmthyln/',
+        icon: '<i class="fab fa-fw fa-linkedin" style="color: #0077B5" aria-hidden="true"></i>',
+    }, {
+        text: 'Polywork',
+        link: 'https://work.timothylin.me/',
+        icon: '<i class="fas fa-fw fa-pen-nib" aria-hidden="true"></i>',
+    },
+];
 </script>
 
 <template>
@@ -32,14 +48,11 @@ const expandMenu = ref(false);
           </p>
 
           <div class="navbar-dropdown">
-            <a class="navbar-item" href="https://github.com/tmthyln/">
-              <i class="fab fa-fw fa-github-square" aria-hidden="true"></i> GitHub
-            </a>
-            <a class="navbar-item" href="https://www.linkedin.com/in/tmthyln/">
-              <i class="fab fa-fw fa-linkedin" style="color: #0077B5" aria-hidden="true"></i> LinkedIn
-            </a>
-            <a class="navbar-item" href="https://work.timothylin.me/">
-              <i class="fas fa-fw fa-pen-nib" aria-hidden="true"></i> Polywork
+            <a v-for="place in placeLinks" :key="place.link" class="navbar-item" :href="place.link">
+              <div class="is-flex is-align-items-center">
+                <span class="icon" v-html="place.icon"></span>
+                <span>{{ place.text }}</span>
+              </div>
             </a>
           </div>
         </div>
