4cf910984d5884c581c8d7f27399617a525d93e9
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-02-18 01:05:08 -0500

    generate posts from list of sources

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,15 +1,18 @@
-// @ts-ignore
-import {frontmatter as bogosortingFrontmatter} from '../pages/posts/bogosorting.md';
-// @ts-ignore
-import {frontmatter as turnLeftFrontmatter} from '../pages/posts/turn-left-at-next-prime.md';
-// @ts-ignore
-import {frontmatter as hawaiiFirstHundredFrontmatter} from '../pages/posts/hawaii-first-hundred-hours.md';
-// @ts-ignore
-import {frontmatter as fancyMealFrontmatter} from '../pages/posts/fancy-meal.md';
 
-export default [
-    {frontmatter: fancyMealFrontmatter, link: '/posts/fancy-meal'},
-    {frontmatter: hawaiiFirstHundredFrontmatter, link: '/posts/hawaii-first-hundred-hours'},
-    {frontmatter: turnLeftFrontmatter, link: '/posts/turn-left-at-next-prime'},
-    {frontmatter: bogosortingFrontmatter, link: '/posts/bogosorting'},
-];
+export default await Promise.all([
+    '../pages/posts/fancy-meal.md',
+    '../pages/posts/hawaii-first-hundred-hours.md',
+    '../pages/posts/turn-left-at-next-prime.md',
+    '../pages/posts/bogosorting.md',
+]
+    .map(async sourcePath => {
+        const module = await import(sourcePath)
+        const pathParts = sourcePath.split('/')
+        const filename = pathParts[pathParts.length - 1]
+
+        return {
+            frontmatter: module.frontmatter,
+            link: `/posts/${filename.split('.')[0]}`,
+        }
+    })
+)
