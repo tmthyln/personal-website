c648205bae42f74ff48bb87c84c139f2b778d1de
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2024-02-18 01:48:14 -0500

    revert to manual post data

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,19 +1,18 @@
+// @ts-ignore
+import {frontmatter as bogosortingFrontmatter} from '../pages/posts/bogosorting.md';
+// @ts-ignore
+import {frontmatter as turnLeftFrontmatter} from '../pages/posts/turn-left-at-next-prime.md';
+// @ts-ignore
+import {frontmatter as hawaiiFirstHundredFrontmatter} from '../pages/posts/hawaii-first-hundred-hours.md';
+// @ts-ignore
+import {frontmatter as fancyMealFrontmatter} from '../pages/posts/fancy-meal.md';
+// @ts-ignore
+import {frontmatter as sacrificeFrontmatter} from '../pages/posts/rigor-sacrifice.md';
 
-export default await Promise.all([
-    '../pages/posts/rigor-sacrifice.md',
-    '../pages/posts/fancy-meal.md',
-    '../pages/posts/hawaii-first-hundred-hours.md',
-    '../pages/posts/turn-left-at-next-prime.md',
-    '../pages/posts/bogosorting.md',
-]
-    .map(async sourcePath => {
-        const module = await import(sourcePath)
-        const pathParts = sourcePath.split('/')
-        const filename = pathParts[pathParts.length - 1]
-
-        return {
-            frontmatter: module.frontmatter,
-            link: `/posts/${filename.split('.')[0]}`,
-        }
-    })
-)
+export default [
+    {frontmatter: sacrificeFrontmatter, link: '/posts/rigor-sacrifice'},
+    {frontmatter: fancyMealFrontmatter, link: '/posts/fancy-meal'},
+    {frontmatter: hawaiiFirstHundredFrontmatter, link: '/posts/hawaii-first-hundred-hours'},
+    {frontmatter: turnLeftFrontmatter, link: '/posts/turn-left-at-next-prime'},
+    {frontmatter: bogosortingFrontmatter, link: '/posts/bogosorting'},
+];
