75d704df2925f74be5523a3ac5e76886d4f56c03
Tim Lin <32549553+tmthyln@users.noreply.github.com>
2025-10-10 22:21:27 -0400

    feat: individual tag pages

--- src/tags/[tag].md
+++ src/tags/[tag].md
@@ -1,2 +1,20 @@
 
 # Posts Tagged with \#{{ $params.tag }}
+
+<script setup lang="ts">
+import { useData } from 'vitepress';
+const { params } = useData();
+
+import {data} from '../posts.data.ts';
+import BlogPostCard from "../components/BlogPostCard.vue";
+
+const taggedPosts = data.filter(post => (post.frontmatter.tags ?? []).includes(params.value.tag));
+</script>
+
+<BlogPostCard
+v-for="post in taggedPosts" :key="post.url"
+:title="post.frontmatter.title"
+:date="post.frontmatter.date"
+:description="post.frontmatter.description"
+:tags="post.frontmatter.tags"
+:link="post.url"/>
--- src/tags/[tag].paths.ts
+++ src/tags/[tag].paths.ts
@@ -1,13 +1,26 @@
-import TagsData from '../tags.data.ts'
+import fs from 'fs';
+import matter from 'gray-matter';
 
-const tagFrequencyMap = TagsData.load()
 export default {
     paths() {
-        return [...tagFrequencyMap.entries()]
-            .map(([tag, count]) => ({
-                params: {
-                    tag,
-                },
-            }))
+        const tagFreqs = fs.readdirSync('src/posts')
+            .filter((filename: string) => filename.endsWith('.md'))
+            .flatMap((filename: string) => {
+                const file = fs.readFileSync(`src/posts/${filename}`, 'utf8')
+                const { data } = matter(file);
+                return data.tags ?? []
+            })
+            .reduce((freqMap, tag) => {
+                if (freqMap.has(tag)) {
+                    freqMap.set(tag, freqMap.get(tag) + 1)
+                } else {
+                    freqMap.set(tag, 1)
+                }
+                return freqMap
+            }, new Map<string, number>())
+
+        return [...tagFreqs]
+            .sort((a, b) => b[1] - a[1])
+            .map(([tag]) => ({params: {tag}}))
     }
 }
